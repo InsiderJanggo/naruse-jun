1d0c3a472fa59cfd941c0ac3c1a2978d2ba6aa14
Wisly ong <52624933+InsiderJanggo@users.noreply.github.com>
2020-08-13 21:59:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,10 @@ module.exports = {
     TOKEN:"",
     PREFIX:"n!",
     OWNER:"ウェスリー オング #4138",
+    OAuth2_URL: "",
+    SCOPE: "",
+    PERM_CODE: "",
+    CLIENT_ID: "",
+    CLIENT_SECRET: "",
+    REDIRECT_URL: "http://localhost:3000" 
 }
